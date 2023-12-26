4b3db300b3dd52dc27857aa0a73a856f2b2f2702
Ivashkka <ivashka.2.r@gmail.com>
2023-12-26 17:02:01 +0300

    sessions settings

--- datastruct.py
+++ datastruct.py
@@ -1,15 +1,22 @@
 import enum
 
 class NAMDtype(enum.Enum): # primary key to transfer data between server and client
-    AIrequest = "AIrequest"
-    AIresponse = "AIresponse"
-    NAMuser = "NAMuser"
+    AIrequest       =   "AIrequest"
+    AIresponse      =   "AIresponse"
+    NAMuser         =   "NAMuser"
+    NAMSesSettings  =   "NAMSesSettings"
+
+class AImodels(enum.Enum):
+    GPT35turbo  =   "gpt_35_turbo"
+    GPT35long   =   "gpt_35_long"
+    GPT4        =   "gpt_4"
+    GPT4turbo   =   "gpt_4_turbo"
 
 #basic classes for user, response, request and session:
 
 class NAMuser:
     __slots__ = ['type', 'name', 'pass_hash', 'uuid']
-    def __init__(self=None, name=None, pass_hash=None, uuid=None):
+    def __init__(self, name=None, pass_hash=None, uuid=None):
         self.type = NAMDtype.NAMuser
         self.name = name
         self.pass_hash = pass_hash
@@ -17,63 +24,30 @@ class NAMuser:
 
 class AIrequest:
     __slots__ = ['message', 'uuid', 'type']
-    def __init__(self=None, message=None, uuid=None):
+    def __init__(self, message=None, uuid=None):
         self.message = message
         self.uuid = uuid
         self.type = NAMDtype.AIrequest
 
 class AIresponse:
     __slots__ = ['message', 'uuid', 'type']
-    def __init__(self=None, message=None, uuid=None):
+    def __init__(self, message=None, uuid=None):
         self.message = message
         self.uuid = uuid
         self.type = NAMDtype.AIresponse
 
-class NAMconnection:
-    __slots__ = ['uuid', 'user', 'client_conn', 'client_addr']
-    def __init__(self, uuid, user, client_conn, client_addr):
-        self.uuid = uuid
-        self.user = user
-        self.client_addr = client_addr
-        self.client_conn = client_conn
-
-class NAMsession:
-    __slots__ = ['uuid', 'client', 'thread', 'messages_history', 'text_history', '__weakref__']
-    count = 0
-    def __init__(self, uuid, client, thread):
-        self.uuid = uuid
-        self.client = client
-        self.thread = thread
-        self.messages_history = [] #list of objects of type message
-        self.text_history = [] #list of dictionaries for g4f requests
-        NAMsession.count += 1
-
-    def add_message(self, message):
-        self.messages_history.append(message)
-        match message.type:
-            case NAMDtype.AIrequest:
-                self.text_history.append({"role": "user", "content": message.message})
-            case NAMDtype.AIresponse:
-                self.text_history.append({"role": "assistant", "content": message.message})
-            case _:
-                pass
-
-    def get_username(self):
-        return self.client.user.name
-    
-    def get_useruuid(self):
-        return self.client.user.uuid
-
-    def get_client_conn(self):
-        return self.client.client_conn
-
+class NAMSesSettings:
+    __slots__ = ['model', 'type']
+    def __init__(self, model=None):
+        self.model = model
+        self.type = NAMDtype.NAMSesSettings
 
 def to_dict(obj): #convert any class object to dictionary
     if not hasattr(obj, 'type'): return
     dict = {}
     for field in obj.__slots__:
         dict[field] = getattr(obj, field)
-        if field == "type":
+        if field == 'type' or field == 'model':
             dict[field] = getattr(obj, field).value
     return dict
 
@@ -84,4 +58,6 @@ def from_dict(dict): #create class object from given dictionary
         setattr(obj, field, dict[field])
         if field == 'type':
             setattr(obj, field, NAMDtype(dict[field]))
+        if field == 'model':
+            setattr(obj, field, AImodels(dict[field]))
     return obj
---
+++ intapi/connect.py
@@ -0,0 +1,48 @@
+import socket
+import json
+
+class _NAMclient(object): #basic clientside networking structure
+    init = False
+    client_sock = None
+    encoding = None
+    server_ip = None
+    server_port = None
+
+    @staticmethod
+    def init_socket(server_ip, server_port, encoding): #create socket
+        _NAMclient.client_sock = socket.socket()
+        _NAMclient.server_ip = server_ip
+        _NAMclient.server_port = server_port
+        _NAMclient.encoding = encoding
+        _NAMclient.init = True
+
+    @staticmethod
+    def connect_to_srv(auth_data, settings): #connect to srv and send auth data
+        if not _NAMclient.init: return
+        _NAMclient.client_sock.connect((_NAMclient.server_ip, _NAMclient.server_port))
+        _NAMclient.send_data({"auth_data": auth_data, "settings": settings})
+
+    @staticmethod
+    def get_data(bytes):
+        if not _NAMclient.init: return {"corrupted": "client was not inited"}
+        return json.loads(_NAMclient.client_sock.recv(bytes).decode())
+    
+    @staticmethod
+    def send_data(data):
+        if not _NAMclient.init: return
+        return _NAMclient.client_sock.send(json.dumps(data).encode(encoding=_NAMclient.encoding))
+
+def init_client(client_settings):
+    _NAMclient.init_socket(client_settings["server_ip"], client_settings["server_port"], client_settings["encoding"])
+
+def connect_to_srv(auth_data, settings):
+    _NAMclient.connect_to_srv(auth_data, settings)
+
+def send_data(data):
+    return _NAMclient.send_data(data)
+
+def get_data(bytes):
+    return _NAMclient.get_data(bytes)
+
+def get_encoding():
+    return _NAMclient.encoding
